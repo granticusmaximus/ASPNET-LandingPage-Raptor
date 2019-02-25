f1bd55d0d95f7b8ca6374bf60d2b2a202d4fad87
Grant Watson <grant@triumphdesigns.org>
2019-02-25 13:24:34 -0500

    Initial

---
+++ coderush22/Controllers/RaptorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coderush22.Controllers
+{
+    public class RaptorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult BlogSingle()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Domain()
+        {
+            return View();
+        }
+
+        public IActionResult Hosting()
+        {
+            return View();
+        }
+    }
+}
